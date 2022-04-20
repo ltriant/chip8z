cc957f69792d4c3174828ca1df94309ab29438b4
Luke Triantafyllidis <ltriant@cpan.org>
2022-04-20 16:57:28 +1000

    core: make a sound when the sound timer is non-zero

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,7 @@ const std = @import("std");
 const fs = std.fs;
 const io = std.io;
 const log = std.log;
+const mem = std.mem;
 const process = std.process;
 const time = std.time;
 
@@ -35,6 +36,9 @@ const MillisPerClock = @floatToInt(i64, (1.0 / ClockRate) * 1000.0);
 const TimerClockRate = 60.0;
 const MillisPerTimerClock = @floatToInt(i64, (1.0 / TimerClockRate) * 1000.0);
 
+// The number of audio samples to produce for a beep
+const SamplesPerBeep = 8192;
+
 fn powerUp(machine: *chip8.Chip8) !void {
     var window = c.SDL_CreateWindow("chip8z", 100, 100, ScreenWidth * ScreenFactor, ScreenHeight * ScreenFactor, c.SDL_WINDOW_SHOWN);
 
@@ -56,114 +60,46 @@ fn powerUp(machine: *chip8.Chip8) !void {
 
         while (c.SDL_PollEvent(&event) != 0) {
             switch (event.type) {
-                c.SDL_QUIT => {
-                    quit = true;
-                },
+                c.SDL_QUIT => quit = true,
                 c.SDL_KEYDOWN => {
                     switch (event.key.keysym.sym) {
-                        c.SDLK_1 => {
-                            machine.keyDown(0x01);
-                        },
-                        c.SDLK_2 => {
-                            machine.keyDown(0x02);
-                        },
-                        c.SDLK_3 => {
-                            machine.keyDown(0x03);
-                        },
-                        c.SDLK_4 => {
-                            machine.keyDown(0x0c);
-                        },
-                        c.SDLK_q => {
-                            machine.keyDown(0x04);
-                        },
-                        c.SDLK_w => {
-                            machine.keyDown(0x05);
-                        },
-                        c.SDLK_e => {
-                            machine.keyDown(0x06);
-                        },
-                        c.SDLK_r => {
-                            machine.keyDown(0x0d);
-                        },
-                        c.SDLK_a => {
-                            machine.keyDown(0x07);
-                        },
-                        c.SDLK_s => {
-                            machine.keyDown(0x08);
-                        },
-                        c.SDLK_d => {
-                            machine.keyDown(0x09);
-                        },
-                        c.SDLK_f => {
-                            machine.keyDown(0x0e);
-                        },
-                        c.SDLK_z => {
-                            machine.keyDown(0x0a);
-                        },
-                        c.SDLK_x => {
-                            machine.keyDown(0x00);
-                        },
-                        c.SDLK_c => {
-                            machine.keyDown(0x0b);
-                        },
-                        c.SDLK_v => {
-                            machine.keyDown(0x0f);
-                        },
-
+                        c.SDLK_1 => machine.keyDown(0x01),
+                        c.SDLK_2 => machine.keyDown(0x02),
+                        c.SDLK_3 => machine.keyDown(0x03),
+                        c.SDLK_4 => machine.keyDown(0x0c),
+                        c.SDLK_q => machine.keyDown(0x04),
+                        c.SDLK_w => machine.keyDown(0x05),
+                        c.SDLK_e => machine.keyDown(0x06),
+                        c.SDLK_r => machine.keyDown(0x0d),
+                        c.SDLK_a => machine.keyDown(0x07),
+                        c.SDLK_s => machine.keyDown(0x08),
+                        c.SDLK_d => machine.keyDown(0x09),
+                        c.SDLK_f => machine.keyDown(0x0e),
+                        c.SDLK_z => machine.keyDown(0x0a),
+                        c.SDLK_x => machine.keyDown(0x00),
+                        c.SDLK_c => machine.keyDown(0x0b),
+                        c.SDLK_v => machine.keyDown(0x0f),
                         else => {},
                     }
                 },
                 c.SDL_KEYUP => {
                     switch (event.key.keysym.sym) {
-                        c.SDLK_1 => {
-                            machine.keyUp(0x01);
-                        },
-                        c.SDLK_2 => {
-                            machine.keyUp(0x02);
-                        },
-                        c.SDLK_3 => {
-                            machine.keyUp(0x03);
-                        },
-                        c.SDLK_4 => {
-                            machine.keyUp(0x0c);
-                        },
-                        c.SDLK_q => {
-                            machine.keyUp(0x04);
-                        },
-                        c.SDLK_w => {
-                            machine.keyUp(0x05);
-                        },
-                        c.SDLK_e => {
-                            machine.keyUp(0x06);
-                        },
-                        c.SDLK_r => {
-                            machine.keyUp(0x0d);
-                        },
-                        c.SDLK_a => {
-                            machine.keyUp(0x07);
-                        },
-                        c.SDLK_s => {
-                            machine.keyUp(0x08);
-                        },
-                        c.SDLK_d => {
-                            machine.keyUp(0x09);
-                        },
-                        c.SDLK_f => {
-                            machine.keyUp(0x0e);
-                        },
-                        c.SDLK_z => {
-                            machine.keyUp(0x0a);
-                        },
-                        c.SDLK_x => {
-                            machine.keyUp(0x00);
-                        },
-                        c.SDLK_c => {
-                            machine.keyUp(0x0b);
-                        },
-                        c.SDLK_v => {
-                            machine.keyUp(0x0f);
-                        },
-
+                        c.SDLK_1 => machine.keyUp(0x01),
+                        c.SDLK_2 => machine.keyUp(0x02),
+                        c.SDLK_3 => machine.keyUp(0x03),
+                        c.SDLK_4 => machine.keyUp(0x0c),
+                        c.SDLK_q => machine.keyUp(0x04),
+                        c.SDLK_w => machine.keyUp(0x05),
+                        c.SDLK_e => machine.keyUp(0x06),
+                        c.SDLK_r => machine.keyUp(0x0d),
+                        c.SDLK_a => machine.keyUp(0x07),
+                        c.SDLK_s => machine.keyUp(0x08),
+                        c.SDLK_d => machine.keyUp(0x09),
+                        c.SDLK_f => machine.keyUp(0x0e),
+                        c.SDLK_z => machine.keyUp(0x0a),
+                        c.SDLK_x => machine.keyUp(0x00),
+                        c.SDLK_c => machine.keyUp(0x0b),
+                        c.SDLK_v => machine.keyUp(0x0f),
                         else => {},
                     }
                 },
@@ -216,9 +152,34 @@ fn powerUp(machine: *chip8.Chip8) !void {
             timerClockNow = now;
 
             if (machine.ST > 0) {
-                log.info("BEEP! BEEP!", .{});
+                beep();
             }
         }
+    }
+}
+
+var playback_device_id: c.SDL_AudioDeviceID = undefined;
+
+fn beep() void {
+    const tone_volume: f32 = 0.1;
+    const period: usize = 44100 / 415; // if it's not Baroque, don't fix it! *groan*
+    var buf = mem.zeroes([SamplesPerBeep]f32);
+
+    var s: usize = 0;
+    while (s < SamplesPerBeep) {
+        if (((s / period) % 2) == 0) {
+            buf[s] = tone_volume;
+        } else {
+            buf[s] = -tone_volume;
+        }
+
+        s += 1;
+    }
+
+    const rv = c.SDL_QueueAudio(playback_device_id, &buf, buf.len);
+
+    if (rv != 0) {
+        log.warn("Unable to queue audio samples: {s}", .{c.SDL_GetError()});
     }
 }
 
@@ -231,11 +192,44 @@ pub fn main() !void {
         process.exit(1);
     }
 
-    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+    if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
         log.warn("Unable to initialise SDL: {s}", .{c.SDL_GetError()});
         return ErrorSet.SDLError;
     }
     defer c.SDL_Quit();
+
+    //
+    // Setup audio
+    //
+
+    var want_spec: c.SDL_AudioSpec = undefined;
+    // 44.1kHz
+    want_spec.freq = 44100;
+    // Prefer f32s
+    want_spec.format = c.AUDIO_F32;
+    // Stereo
+    want_spec.channels = 2;
+    // The number of samples to hold in the buffer
+    want_spec.samples = 1024;
+    // Prefer to queue audio samples
+    want_spec.callback = null;
+
+    var have_spec: c.SDL_AudioSpec = undefined;
+
+    playback_device_id = c.SDL_OpenAudioDevice(null, c.SDL_FALSE, &want_spec, &have_spec, c.SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+
+    if (playback_device_id == 0) {
+        log.warn("Unable to initialise audio: {s}", .{c.SDL_GetError()});
+    } else {
+        log.info("Device ID: {}, channels: {}, freq: {} Hz, samples: {}", .{ playback_device_id, have_spec.channels, have_spec.freq, have_spec.samples });
+    }
+
+    // Unpause the audio device, otherwise nothing will play...
+    c.SDL_PauseAudioDevice(playback_device_id, c.SDL_FALSE);
+
+    //
+    // Read the ROM data
+    //
 
     const dir = fs.cwd();
     const file = try dir.openFile(args[1], .{ .read = true });
@@ -245,6 +239,10 @@ pub fn main() !void {
     const reader = file.reader();
     const nBytes = try reader.readAll(prgRom[0..]);
     log.info("Read {} bytes of PRG ROM", .{nBytes});
+
+    //
+    // Create the CHIP-8 machine and go!
+    //
 
     var machine = chip8.Chip8.init(prgRom[0..nBytes]);
     try powerUp(&machine);
